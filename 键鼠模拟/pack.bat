4adb378bebba7f45aa4e13802d5f2a93985de4be
Bob Li <2137610668@qq.com>
2024-12-12 23:14:42 +0800

    fix(simulator_app):v1.1.5 1.新增精简页面; 2.完善代码注释

--- 键鼠模拟/pack.bat
+++ 键鼠模拟/pack.bat
@@ -5,7 +5,7 @@ rem 指定要打包的 Python 脚本列表
 set SCRIPTS=simulator_app.py
 
 rem 设置额外字段（可以根据需求修改）
-set ADDITIONAL_FIELD=_v1.1.0
+set ADDITIONAL_FIELD=_v1.1.5
 
 rem 组合成最终的输出文件名
 set OUTPUT_NAME=%BASENAME%%ADDITIONAL_FIELD%
@@ -16,7 +16,7 @@ set OPTIONS=--onefile --windowed
 rem 遍历每个脚本进行打包
 for %%s in (%SCRIPTS%) do (
     echo 正在打包 %%s ...
-    pyinstaller %OPTIONS% %%s --name=%%~ns%ADDITIONAL_FIELD% 
+    pyinstaller %OPTIONS% %%s --name=%%~ns%ADDITIONAL_FIELD%
     echo %%s 打包完成，输出文件名为 %OUTPUT_NAME%.
 )
 
