cc8a95e1bebb7329fb586fafb13dd96b3d2d7734
Bob Li <2137610668@qq.com>
2024-12-31 11:18:36 +0800

    fix(pack.bat)

--- 键鼠模拟/pack.bat
+++ 键鼠模拟/pack.bat
@@ -4,8 +4,8 @@ setlocal
 rem 指定要打包的 Python 脚本列表
 set SCRIPTS=simulator_app.py
 
-rem 设置额外字段（可以根据需求修改）
-set ADDITIONAL_FIELD=_v1.1.5
+rem 设置额外字段（根据需求修改）
+set ADDITIONAL_FIELD=_v1.1.6
 
 rem 组合成最终的输出文件名
 set OUTPUT_NAME=%BASENAME%%ADDITIONAL_FIELD%
@@ -15,9 +15,9 @@ set OPTIONS=--onefile --windowed
 
 rem 遍历每个脚本进行打包
 for %%s in (%SCRIPTS%) do (
-    echo 正在打包 %%s ...
+    echo %%s ...
     pyinstaller %OPTIONS% %%s --name=%%~ns%ADDITIONAL_FIELD%
-    echo %%s 打包完成，输出文件名为 %OUTPUT_NAME%.
+    echo %%s %OUTPUT_NAME%.
 )
 
 pause
